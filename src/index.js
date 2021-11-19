6ee3f9dea8eb3d211eaa8a8562ecfc32af2582ee
amanda <amanda.blaze1988@gmail.com>
2021-11-18 18:12:54 -0800

    Created Reducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import { reducer } from './redux/reducer'
+import thunk from 'redux-thunk'
+
+
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(
+applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
