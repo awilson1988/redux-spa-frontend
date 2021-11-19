66dc4654ecf6e3c99f6bb816697c54b9d5feedff
amanda <amanda.blaze1988@gmail.com>
2021-11-18 18:17:35 -0800

    Fixed Bug

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux';
-import { reducer } from './redux/reducer'
+import reducer from './redux/reducer'
 import thunk from 'redux-thunk'
 
 
-const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(
 applyMiddleware(thunk)
