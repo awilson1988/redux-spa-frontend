5cc339aa154043b98551c97791cd7b2669b54aed
amanda <amanda.blaze1988@gmail.com>
2022-01-05 22:57:13 -0800

    Created SortedServiceCard

--- src/components/SortedServiceCards.js
+++ src/components/SortedServiceCards.js
@@ -1,7 +1,7 @@
 // import {getServices} from '../redux/actionCreators'
 
 function SortedServiceCards() {
-    console.log("hi")
+    console.log("Hello World")
     
     // const sortServices = () => {
     //     {services.sort((a, b) => (a.duration > b.duration) ? 1 : -1)}
